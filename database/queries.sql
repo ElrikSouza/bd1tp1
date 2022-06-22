e847ab066be424d75f31ec0c536f92724e863ddb
foliveirafilho <franciscofilho1109@gmail.com>
2022-06-22 13:44:16 -0400

    fix queries & add D query

--- database/queries.sql
+++ database/queries.sql
@@ -4,43 +4,45 @@
 (
   SELECT *
   FROM review
-  WHERE productAsin = 111
-  ORDER BY helpful DESC, rating DESC
+  WHERE product_asin = '111'
+  ORDER BY rating DESC, helpful DESC
   LIMIT 5
 )
 UNION
 (
   SELECT *
   FROM review
-  WHERE productAsin = 111
-  ORDER BY helpful DESC, rating ASC
+  WHERE product_asin = '111'
+  ORDER BY rating DESC, helpful ASC
   LIMIT 5
 );
 
 -- consulta B
 -- Dado um produto, listar os produtos similares com maiores vendas do que ele
-SELECT ps.asin, ps.groupId, ps.title, ps.sales_rank
-FROM similarTo st
-JOIN product po ON po.asin = st.productOriginAsin
-JOIN product ps ON ps.asin = st.productSimilarAsin
-WHERE (st.productOriginAsin = 111) AND (ps.sales_rank > po.sales_rank);
+SELECT ps.*
+FROM similar_to st
+LEFT JOIN product po ON po.asin = st.product_origin_asin
+LEFT JOIN product ps ON ps.asin = st.product_similar_asin
+WHERE (st.product_origin_asin = '0827229534') AND (ps.sales_rank > po.sales_rank);
 
 -- consulta C
 -- Dado um produto, mostrar a evolução diária das médias de avaliação
 -- ao longo do intervalo de tempo coberto no arquivo de entrada
-SELECT p.title, r.reviewedAt, AVG(r.rating) as rating_avg
+SELECT p.title, r.reviewed_at, AVG(r.rating) as rating_avg
 FROM product p
-JOIN review r ON p.asin = r.productAsin
-WHERE asin = 111
-GROUP BY p.title, r.reviewedAt
-
-
--- As consultas abaixo tem todas a mesma essência de n-per-group
--- ver: https://stackoverflow.com/questions/1442527/how-to-select-the-newest-four-items-per-category/1442867#1442867
--- ver: https://stackoverflow.com/questions/2129693/using-limit-within-group-by-to-get-n-results-per-group
+LEFT JOIN review r ON p.asin = r.product_asin
+WHERE asin = '111'
+GROUP BY p.title, r.reviewed_at
 
 -- consulta D
 -- Listar os 10 produtos líderes de venda em cada grupo de produtos
+SELECT *
+FROM (
+  SELECT *, rank() OVER (PARTITION BY product_group_id ORDER BY sales_rank ASC) ranked_product_group
+  FROM product
+) as ranked_products
+WHERE ranked_product_group <= 10 AND product_group_id IS NOT NULL
+ORDER BY product_group_id
 
 -- consulta E
 -- Listar os 10 produtos com a maior média de avaliações úteis positivas por produto
